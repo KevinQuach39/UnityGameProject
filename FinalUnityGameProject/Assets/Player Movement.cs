0f5524688841a682ea93e342fe5ad864d21e9fda
KevinQuach39 <kq1045016@student.francistuttle.edu>
2025-03-31 15:23:32 -0500

    3/31
    Imported robot. Worked on animations. Improved movement script

--- FinalUnityGameProject/Assets/Player Movement.cs
+++ FinalUnityGameProject/Assets/Player Movement.cs
@@ -4,34 +4,47 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-   
     CharacterController characterController;
-    public float speed = 5f;
-    private Vector3 direction;
-    
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float rotationSpeed = 8f; // Controls smoothness of rotation
+    [SerializeField] private Vector3 moveDirection;
+    [SerializeField] private Animator animator;
+
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
+
     void Update()
     {
-      float forwardInput = Input.GetAxis("Vertical");
-      float rightInput = Input.GetAxis("Horizontal");
-      Vector3 forward = Camera.main.transform.forward;
-      Vector3 right = Camera.main.transform.right;
+        float forwardInput = Input.GetAxis("Vertical");
+        float rightInput = Input.GetAxis("Horizontal");
 
+        // Fix: Swap forward and right inputs and negate the right input for correct rotation
+        moveDirection = new Vector3(rightInput, 0, -forwardInput); 
 
-        forward.y = 0f;
-        right.y = 0f;
-        forward.Normalize();
-        right.Normalize();
+        if (moveDirection == Vector3.zero)
+        {
+            animator.SetFloat("Speed", 0);
+        }
+        else if (!Input.GetKey(KeyCode.W))
+        {
+            animator.SetFloat("Speed", 0.5f);
+        }
+        else
+        {
+            animator.SetFloat("Speed", 1);
+        }
 
-        direction = (forwardInput * forward) + (rightInput * right);
-        direction.Normalize();
-        direction.y = 0f;
-        characterController.Move(direction * speed * Time.deltaTime);
-        
+        moveDirection *= speed;
+
+        // Rotation fix with proper direction
+        if ((moveDirection.x != 0) || (moveDirection.z != 0))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveDirection), Time.deltaTime * rotationSpeed);
+        }
+
+        characterController.Move(moveDirection * Time.deltaTime);
     }
-    
-    
 }

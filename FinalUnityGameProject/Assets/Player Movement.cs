e2c4e9b9a8de7287e4be4ce3dd58ae98806b57a6
KevinQuach39 <kq1045016@student.francistuttle.edu>
2025-03-31 17:06:37 -0500

    Editing

--- FinalUnityGameProject/Assets/Player Movement.cs
+++ FinalUnityGameProject/Assets/Player Movement.cs
@@ -2,49 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class CharacterMovement : MonoBehaviour
 {
-    CharacterController characterController;
-    [SerializeField] private float speed = 5f;
-    [SerializeField] private float rotationSpeed = 8f; // Controls smoothness of rotation
-    [SerializeField] private Vector3 moveDirection;
-    [SerializeField] private Animator animator;
+    private CharacterController characterController;
+    [SerializeField] private float moveSpeed = 5f;
+    private Vector3 moveDirection;
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
-        animator = GetComponent<Animator>();
     }
-
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
         float rightInput = Input.GetAxis("Horizontal");
+        Vector3 forward = Camera.main.transform.forward;
+        Vector3 right = Camera.main.transform.right;
 
-        // Fix: Swap forward and right inputs and negate the right input for correct rotation
-        moveDirection = new Vector3(rightInput, 0, -forwardInput); 
-
-        if (moveDirection == Vector3.zero)
-        {
-            animator.SetFloat("Speed", 0);
-        }
-        else if (!Input.GetKey(KeyCode.W))
-        {
-            animator.SetFloat("Speed", 0.5f);
-        }
-        else
-        {
-            animator.SetFloat("Speed", 1);
-        }
-
-        moveDirection *= speed;
-
-        // Rotation fix with proper direction
-        if ((moveDirection.x != 0) || (moveDirection.z != 0))
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveDirection), Time.deltaTime * rotationSpeed);
-        }
-
-        characterController.Move(moveDirection * Time.deltaTime);
+        forward.y = 0f;
+        right.y = 0f;
+        forward.Normalize();
+        right.Normalize();
+      
+        moveDirection = (forwardInput * forward) + (rightInput * right);
+        moveDirection.Normalize();
+        moveDirection.y = 0f;
+        characterController.Move(moveDirection * moveSpeed * Time.deltaTime);
     }
 }

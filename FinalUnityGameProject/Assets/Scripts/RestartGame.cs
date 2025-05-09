3cbe5b85076a912ca0900850fcb0a6e9ebf85bb6
KevinQuach39 <kq1045016@student.francistuttle.edu>
2025-05-08 22:13:38 -0500

    5/8

--- FinalUnityGameProject/Assets/Scripts/RestartGame.cs
+++ FinalUnityGameProject/Assets/Scripts/RestartGame.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-public class GameManager : MonoBehaviour
+public class GameOverManager : MonoBehaviour
 {
     public void RestartGame()
     {

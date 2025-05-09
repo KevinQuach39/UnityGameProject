566ab136b567df7806ad6985c1a6036b479a6cf7
KevinQuach39 <kq1045016@student.francistuttle.edu>
2025-05-09 12:30:24 -0500

    5/9

--- FinalUnityGameProject/Assets/Scripts/RestartGame.cs
+++ FinalUnityGameProject/Assets/Scripts/RestartGame.cs
@@ -1,9 +1,53 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class GameOverManager : MonoBehaviour
+
+public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
+    public GameObject menuUI;    
+    public GameObject gameplayUI;  
+    public GameObject gameOverUI; 
+    public GameObject player;     
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+    private void Start()
+    {
+        ShowMenu();
+    }
+    public void ShowMenu()
+    {
+        Time.timeScale = 0f; 
+        menuUI.SetActive(true);    
+        gameplayUI.SetActive(false);
+        gameOverUI.SetActive(false); 
+        player.SetActive(false);    
+    }
+    public void StartGame()
+    {
+        Time.timeScale = 1f; 
+        menuUI.SetActive(false);     
+        gameplayUI.SetActive(true);  
+        gameOverUI.SetActive(false); 
+        player.SetActive(true);
+    }
+    public void GameOver()
+    {
+        Time.timeScale = 0f;         
+        gameOverUI.SetActive(true); 
+    }
     public void RestartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1f;       
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+    }
+    public void QuitGame()
+    {
+        Application.Quit(); 
+        print("Quit Game");
     }
 }

50fe75f7fc16b4677a3e663ef64ddfc6583b8e02
KevinQuach39 <kq1045016@student.francistuttle.edu>
2025-05-09 22:58:25 -0500

    5/9
    saving before i mess around

--- FinalUnityGameProject/Assets/Scripts/RestartGame.cs
+++ FinalUnityGameProject/Assets/Scripts/RestartGame.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameOverManager : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     [Header("UI Panels")]
     public GameObject startScreenPanel;
